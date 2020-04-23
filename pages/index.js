c86f17fe891c1b22ddf40f771d74b0889b8695dd
Souvik <souvikde.ns@gmail.com>
2020-04-23 19:46:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import tinyTime from 'tinytime'
-import { makeStyles } from '@material-ui/core'
+import {
+    makeStyles,
+    Typography,
+    Container,
+    Card,
+    Grid,
+    ThemeProvider
+} from '@material-ui/core'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
-        minWidth: 275
+        flexGrow: 1,
+        marginTop: theme.spacing(10)
+    },
+    card: {
+        padding: theme.spacing(2),
     }
-})
+}))
 
 export default () => {
 
@@ -13,6 +24,28 @@ export default () => {
 
     return (
         <div>
+            <Grid container className={classes.root} spacing={0}>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={1}>
+                        <Grid item>
+                            <Card className={classes.card}>
+                                {tinyTime('{DD}').render(new Date)}
+                            </Card>
+                        </Grid>
+                        <Grid item>
+                            <Card className={classes.card}>
+                                {tinyTime('{MMMM}').render(new Date)}
+                            </Card>
+                        </Grid>
+                        <Grid item>
+                            <Card className={classes.card}>
+                                {tinyTime('{YYYY}').render(new Date)}
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+            </Grid>
         </div>
     )
 }
